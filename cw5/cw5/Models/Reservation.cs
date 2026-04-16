d8b0fe2654b61c9fc550d8b37be9ea34259fee98
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-04-16 19:42:47 +0200

    Utworzenie i uzupełnienie klasy Reservation.cs oraz poprawienie klasy Romm.cs

---
+++ cw5/cw5/Models/Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cw5.Models;
+
+public class Reservation
+{
+   public int Id { get; set; }
+   
+   [Required]
+   public int RoomId { get; set; }
+   
+   [Required]
+   public string OrganizerName { get; set; }
+   
+   [Required]
+   public string Topic { get; set; }
+   
+   [Required]
+   public DateOnly Date { get; set; }
+   
+   [Required]
+   public TimeSpan StartTime { get; set; }
+   
+   [Required]
+   public TimeSpan EndTime { get; set; }
+   
+   [Required]
+   public string Status { get; set; }
+
+   public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+   {
+      if (EndTime <= StartTime)
+      {
+         yield return new ValidationResult("EndTime must be after StartTime and EndTime", new[] { nameof(EndTime) });
+      }
+   }
+   
+}
